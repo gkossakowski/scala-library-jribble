43d8d45a313120dd9120792d87c3c9cf796c8f19
Grzegorz Kossakowski <grzegorz.kossakowski@gmail.com>
2011-01-03 22:25:21 +0100

    Initial commit. Fresh scala-library-jribble.

---
+++ library/scala/runtime/ArrayRuntime.java
@@ -0,0 +1,16 @@
+package scala.runtime;
+
+/**
+ * Methods on Java arrays
+ */
+class ArrayRuntime {
+  static boolean[] cloneArray(boolean[] array) { throw new RuntimeException("array.clone"); }
+  static byte[] cloneArray(byte[] array) { throw new RuntimeException("array.clone"); }
+  static short[] cloneArray(short[] array) { throw new RuntimeException("array.clone"); }
+  static char[] cloneArray(char[] array) { throw new RuntimeException("array.clone"); }
+  static int[] cloneArray(int[] array) { throw new RuntimeException("array.clone"); }
+  static long[] cloneArray(long[] array) { throw new RuntimeException("array.clone"); }
+  static float[] cloneArray(float[] array) { throw new RuntimeException("array.clone"); }
+  static double[] cloneArray(double[] array) { throw new RuntimeException("array.clone"); }
+  static Object[] cloneArray(Object[] array) { throw new RuntimeException("array.clone"); }
+}
